1092338ccae108e3c8ff092c92d40974e3315e28
Mostafa <93074310+tabatabaei-mosi@users.noreply.github.com>
2023-08-01 18:51:07 +0330

    Update test.py
    adjust some parts of code to follow best practices. Minor changes was done on the file. Some parts need more improvement, but doesn't matter, right now. 
    
    Signed-off-by: Mostafa <93074310+tabatabaei-mosi@users.noreply.github.com>

--- src/test/test.py
+++ src/test/test.py
@@ -2,40 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import sys
-sys.path.append('..')
 
 from mealpy.swarm_based import SSA
 from mealpy.evolutionary_based import GA
 from model import ROA
 
+# Add parent directory to the module search path
+sys.path.append('..')
 
-def Knapsack(model, epoch=100, pop_size=50):
+def knapsack(model, epoch=100, pop_size=50):
     """
+    Solves the 0/1 Knapsack Problem using a given optimization algorithm.
+    
     Args:
-        model (module): an optimization algorithm module
+        model (obj): an optimization algorithm
         epoch (int): number of iterations, default = 100
         pop_size (int): population size, default = 50
 
     Return:
-        a runned model
+        obj: A handler to the optimization model.
 
-    References:
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    [+] References:
         https://developers.google.com/optimization/bin/knapsack
     """
+    
+    # Values of items (e.g., profits, revenues, etc.)
     VALUES = np.array([
         360, 83, 59, 130, 431, 67, 230, 52, 93, 125, 670, 892, 600, 38, 48, 147,
         78, 256, 63, 17, 120, 164, 432, 35, 92, 110, 22, 42, 50, 323, 514, 28,
         87, 73, 78, 15, 26, 78, 210, 36, 85, 189, 274, 43, 33, 10, 19, 389, 276,
         312
     ])
+    
+    # Weights of items (e.g., weights in a knapsack problem)
     WEIGHTS = np.array([
         7, 0, 30, 22, 80, 94, 11, 81, 70, 64, 59, 18, 0, 36, 3, 8, 15, 42, 9, 0,
         42, 47, 52, 32, 26, 48, 55, 6, 29, 84, 2, 4, 18, 56, 7, 29, 93, 44, 71,
         3, 86, 66, 31, 65, 0, 79, 20, 65, 52, 13
     ])
+    
+    # Capacity constraint (e.g., maximum capacity of a knapsack)
     CAPACITY = 850
 
-    ## 50 dimensions since we have 50 items
+    ## 50 items >> 50 dimensions
     ## each item has two state: 0 (not put in the bag), 1 (put in the bag)
     ## so lower bound is 0 and upper bound is 1.99 because
     ## int(0 -> 0.99) = 0
@@ -44,62 +54,86 @@ def Knapsack(model, epoch=100, pop_size=50):
     UB = [1.99] * 50
 
     def fitness_function(solution):
+        """
+        Calculates the fitness value for the given solution.
+
+        Args:
+            solution (np.array): Binary array representing the selected items.
+
+        Returns:
+            float: Fitness value of the solution.
+        """
         def punish_function(value):
             """
-            Using this function to handling constraint optimization problem
+            Handles constraint optimization problem by penalizing violations.
+
+            Args:
+                current_weight (float): Total weight of selected items.
+
+            Returns:
+                float: Penalty value (0 if the constraint is satisfied).
             """
             return 0 if value <= CAPACITY else value
 
-        solution_int = solution.astype(int)                 # Convert float to integer here
+        # Convert float to integer to make a binary decision (0 or 1)
+        solution_int = solution.astype(int)                 
         current_capacity = np.sum(solution_int * WEIGHTS)
         temp = np.sum(solution_int * VALUES) - punish_function(current_capacity)
+        
         return temp
 
-
+    # Define the problem dictionary for the optimize
     problem_dict = {
         "fit_func": fitness_function,
         "lb": LB,
         "ub": UB,
         "minmax": "max",
     }
-    # TODO: log the epoch results to file
-
-    ## Run the algorithm
+    
+    # Build the optimizer model
     model = model(epoch=epoch, pop_size=pop_size)
+    
+    # Optimize the model by solve method
     best_position, best_fitness = model.solve(problem_dict)
+
+    TODO: log the epoch results to file
     return model
 
-def Eggcreate(model, epoch, pop_size):
+
+def egg_crate(model, epoch, pop_size):
     """
     a mathematicall function with global_minimum = 0
 
     Args:
-        model (module): an optimization algorithm module
+        model (obj): an optimization algorithm module
         epoch (int): number of iterations, default = 100
         pop_size (int): population size, default = 50
 
     Return:
-        a runned model
+        obj: A handler to the optimization model.
     """
     def fitness_function(solution):
         return np.sum(solution**2 + 25*np.sin(solution)**2)
 
+    # Define the problem dictionary for the optimize
     problem_dict = {
-        "fit_func": fitness_function, # objective function
-        "lb": [-5, ] * 3,  # Lower bounds for X and Y
-        "ub": [5, ] * 3,   # Upper bounds for X and Y
-        'minmax': 'min'   # Minimization or Maximization
+        "fit_func": fitness_function,
+        "lb": [-5, ] * 3,  
+        "ub": [5, ] * 3,   
+        'minmax': 'min'   
         }
-        # TODO: log the epoch results to file
 
-
+    # Build the optimizer model
     model = model(epoch=epoch, pop_size=pop_size)
+    
+    # Optimize the model by solve method
     best_position, best_fitness = model.solve(problem_dict)
+    
     return model
 
-def Shubert(model, epoch, pop_size):
+def shubert(model, epoch, pop_size):
     """
-    a mathematicall function with global_minimum = -186.73
+    A mathematical function with global_minimum = -186.73.
 
     Args:
         model (module): an optimization algorithm module
@@ -120,108 +154,146 @@ def Shubert(model, epoch, pop_size):
 
         return sum_part_1*sum_part_2
 
+    # Define the problem dictionary for the optimizer
     problem_dict = {
         "fit_func": fitness_function, # objective function
         "lb": [-10, ] * 2,  # Lower bounds for X and Y
         "ub": [10, ] * 2,   # Upper bounds for X and Y
         'minmax': 'min'   # Minimization or Maximization
         }
-        # TODO: log the epoch results to file
+    
+    # TODO: Log the epoch results to a file
 
-    model = model(epoch=epoch, pop_size=pop_size)
-    best_position, best_fitness = model.solve(problem_dict)
+    # Build the optimizer model
+    optimizer_model = optimizer(epoch=epoch, pop_size=pop_size)
+
+    # Optimize the model using the solve method
+    best_position, best_fitness = optimizer_model.solve(problem_dict)
+    
     return model
 
 class TestEvaluation:
+    @staticmethod
+    def time_report(time):
+        """
+        Convert the given time to a formatted string with appropriate units for reporting.
+
+        Args:
+            time (float): Time value in seconds.
+
+        Returns:
+            str: A formatted string representing the time in the format "X h Y min Z sec".
+        """
+        hours = int(time // 3600)
+        minutes = int((time % 3600) // 60)
+        seconds = int(time % 60)
+
+        time_string = ""
+        if hours > 0:
+            time_string += f"{hours} h "
+        if minutes > 0:
+            time_string += f"{minutes} min "
+        if seconds > 0 or not time_string:
+            time_string += f"{seconds} sec"
+
+        return time_string
+        
     def __init__(self, models):
         """
-            Initialize the evaluation class
+        The class will compare different optimizer algorithm to monitor the performance
         
-            Args:
-                models (dictionary): a dictionary of runned models:
-                    - keys : names
-                    - values : runned models
+        Args:
+            models (dic): a dictionary of optimizer models:
+                - keys : names
+                - values : runned models
         """
+        
         self.models = models
 
     def CompareModels(self, title):
         """
-            Compare some models together and report fitness value plot, ouputs/
+        Compare some models together and report fitness value plot, ouputs/
         
-            Args:
-                title (str): name of problem
+        Args:
+            title (str): name of problem
         """
+        
         # get the models and their's name from dictionary
         models = list(self.models.values())
         names = list(self.models.keys())
 
-        # store fitness values and runtime of each model
+        # Store fitness values and runtimes of each model
         fitness_values_list = []
         runtimes = []
-        for i in range(len(models)):
-            fitness_values_list.append(models[i].history.list_global_best_fit)
-            runtimes.append(sum(models[i].history.list_epoch_time))
-            print(f'{names[i]} --> runtime = {round(runtimes[-1], 2)} seconds, Best fitness value = {models[i].solution[1][0]}')
-
+        for model in models:
+            fitness_values_list.append(model.history.list_global_best_fit)
+            runtimes.append(sum(model.history.list_epoch_time))
+            time_string = self.time_report(runtimes[-1])
+            # TODO: Use Logging module
+            print(f'{names[i]} --> runtime = {time_string}, Best fitness value = {models[i].solution[1][0]}')
+            
         epoch = len(models[0].history.list_epoch_time)
         epoch_list = [i for i in range(epoch)]
 
         # plot global best ftiness values for each model and save in src/test/outputs/
         for i in range(len(models)):
             plt.plot(epoch_list, fitness_values_list[i], label=f'{names[i]}')
+            
         plt.title(title)
         plt.xlabel('Iteration')
         plt.ylabel('Fitness value')
         plt.legend()
         plt.savefig(f'outputs/{title}-fitness_values.png')
         plt.cla()
+            
+        
 
 
 if __name__ == '__main__':
-    # run some algorithms for Knapsack problem
-    roa = Knapsack(ROA.ROA, epoch=100, pop_size=50)
-    ssa = Knapsack(SSA.BaseSSA, epoch=100, pop_size=50)
-    ga = Knapsack(GA.BaseGA, epoch=100, pop_size=50)
+    # Optimize Knapsack problem with 3 optimizer algorithm
+    roa_knp = knapsack(ROA.ROA, epoch=100, pop_size=50)
+    ssa_knp = knapsack(SSA.BaseSSA, epoch=100, pop_size=50)
+    ga_knp = knapsack(GA.BaseGA, epoch=100, pop_size=50)
 
 
-    # Compare runned models together 
+    # define a dictionary of optimizer models 
     models = {
-        'ROA': roa,
-        'GA': ga,
-        'SSA': ssa
+        'ROA': roa_knp, # Rain Optimization Algorithm
+        'GA': ga_knp,   # Genetic Algorithm
+        'SSA': ssa_knp  # Sparrow Search Algorithm
     }
 
+    # TODO: Log the result
     print("Knapsack problem : ")
     test = TestEvaluation(models=models)
     test.CompareModels('Knapsack Problem')
 
+    # Optimize Eggcreate benchmark with 3 optimizer algorithm
+    roa_ec = egg_crate(ROA.ROA, epoch=100, pop_size=50)
+    ga_ec = egg_crate(GA.BaseGA, epoch=100, pop_size=50)
+    ssa_ec = egg_crate(SSA.BaseSSA, epoch=100, pop_size=50)
 
-    # run some algorithms for Eggcreate benchmark
-    roa = Eggcreate(ROA.ROA, epoch=100, pop_size=50)
-    ga = Eggcreate(GA.BaseGA, epoch=100, pop_size=50)
-    ssa = Eggcreate(SSA.BaseSSA, epoch=100, pop_size=50)
-
-    # Compare runned models together 
+    # model dictionary 
     models = {
-        'ROA': roa,
-        'GA': ga,
-        'SSA': ssa,
+        'ROA': roa_ec,
+        'GA': ga_ec,
+        'SSA': ssa_ec,
     }
 
-    print("Eggcreate problem : ")
+    print("Egg Crate problem : ")
     test = TestEvaluation(models=models)
     test.CompareModels('Eggcreate Problem')
 
     # run some algorithms for Shubert equation
-    roa = Shubert(ROA.ROA, epoch=100, pop_size=50)
-    ga = Shubert(GA.BaseGA, epoch=100, pop_size=50)
-    ssa = Shubert(SSA.BaseSSA, epoch=100, pop_size=50)
+    roa_sh = shubert(ROA.ROA, epoch=100, pop_size=50)
+    ga_sh = shubert(GA.BaseGA, epoch=100, pop_size=50)
+    ssa_sh = shubert(SSA.BaseSSA, epoch=100, pop_size=50)
 
-    # Compare runned models together 
+    # model dictionary 
     models = {
-        'ROA': roa,
-        'GA': ga,
-        'SSA': ssa,
+        'ROA': roa_sh,
+        'GA': ga_sh,
+        'SSA': ssa_sh,
     }
 
     print("Shubert problem : ")
