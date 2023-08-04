8f5407bc352f60f3f0b9f2b2e2414aafd49c83dd
MmdAzadeh <mohammad.azadeh@ut.ac.ir>
2023-08-04 19:23:55 +0330

    implement BaseROA and its methods in src/model/ROA.py

--- src/model/ROA.py
+++ src/model/ROA.py
@@ -2,47 +2,53 @@ import numpy as np
 from mealpy.optimizer import Optimizer
 
 
-class ROA(Optimizer):
+class BaseROA(Optimizer):
     """
-    The developed version: Rain Optimization Algorithm (ROA)
+    The developed version: Base Rain Optimization Algorithm (ROA)
 
     References
     ~~~~~~~~~~
     [1] Ali Reza Moazzeni, Ehsan Khamehchi 2020. Rain optimization algorithm (ROA): A new metaheuristic method 
     for drilling optimization solutions, DOI: https://doi.org/10.1016/j.petrol.2020.107512
     """
-    def __init__(self, epoch, pop_size,
-                init_radius, joint_size, rain_speed, soil_adsorption,
-                **kwargs):
+
+    def __init__(self, epoch=100, pop_size=50,
+                 init_radius=1, joint_size=1, rain_speed=0.01, soil_adsorption=5, r_min=0.00001,
+                 **kwargs):
         """
         Initialize the Rain Optimization Algorithm (ROA) optimizer.
 
         Args:
-            epoch (int): Maximum number of iterations.
+            epoch (int): Maximum number of iterations. 
             pop_size (int): Population size.
-            init_radius (float): Initial radius of raindrops.
+            init_radius (float): Initial radius of raindrops. default = 0.0001
             joint_size (float): Joint size for merging raindrops.
-            rain_speed (float): Rain speed for moving raindrops. usually [1E-6, 1E-3]
-            soil_adsorption (float): Soil adsorption constant for raindrops absorption.
+            rain_speed (float): Rain speed for moving raindrops. (fraction of self.pop_size), [0, 1]
+            soil_adsorption (float): Soil adsorption constant for raindrops absorption (percentage). [0, 100]
+            r_min (float): Minimum possible radius for droplet. [0, init_radius]
 
         Returns:
             None
         """
         super().__init__(**kwargs)
 
-        #Check the validation range for each hyper-parameters
+        # Check the validation range for each hyper-parameters
         self.epoch = self.validator.check_int('epoch', epoch, [1, 100000])
-        self.pop_size = self.validator.check_int('pop_size', pop_size, [10, 10000])
-        self.init_radius = self.validator.check_float('init_radius', init_radius)
+        self.pop_size = self.validator.check_int(
+            'pop_size', pop_size, [10, 10000])
+        self.init_radius = self.validator.check_float(
+            'init_radius', init_radius, [0, 1])
         self.joint_size = self.validator.check_float('joint_size', joint_size)
-        self.rain_speed = self.validator.check_float('rain_speed', rain_speed)
-        self.soil_adsorption = self.validator.check_float('soil_adsorption', soil_adsorption)
+        self.rain_speed = self.validator.check_float('rain_speed', rain_speed, [0, 1])
+        self.soil_adsorption = self.validator.check_float(
+            'soil_adsorption', soil_adsorption, [0, 100])
+        self.r_min = self.validator.check_float('r_min', r_min, [0, self.init_radius])
 
+        # Determine to sort the problem or not in each epoch
+        # if True, the problem always sorted with fitness value increase
+        # if False, the problem is not sorted
         self.nfe_per_epoch = self.pop_size
         self.sort_flag = True
-        # Determine to sort the problem or not in each epoch
-        ## if True, the problem always sorted with fitness value increase
-        ## if False, the problem is not sorted
 
     def initialize_variables(self):
         """
@@ -51,7 +57,7 @@ class ROA(Optimizer):
         """
         # Generate a radius of first population
         self.radius = np.full(self.pop_size, self.init_radius)
-
+        self.size = np.full(self.pop_size, self.joint_size)
 
     def initialization(self):
         """
@@ -61,3 +67,147 @@ class ROA(Optimizer):
         # Required code of mealpy Optimizer
         if self.pop is None:
             self.pop = self.create_population(self.pop_size)
+
+    def evolve(self, epoch):
+        """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
+        Args:
+            epoch (int): The current iteration
+        """
+        for i in range(self.pop_size):
+            for j in range(self.problem.n_dims):
+
+                # Change each variable Xi to Xi + R and Xi - R and evaluate the new position by the objective function
+                # Change Xi to Xi + R
+                new_position_1 = np.copy(self.pop[i][self.ID_POS])
+                new_position_1[j] += self.radius[i]
+                new_position_1 = np.clip(
+                    new_position_1, self.problem.lb, self.problem.ub)
+
+                # Change Xi to Xi - R
+                new_position_2 = np.copy(self.pop[i][self.ID_POS])
+                new_position_2[j] -= self.radius[i]
+                new_position_2 = np.clip(
+                    new_position_2, self.problem.lb, self.problem.ub)
+
+                new_cost_1 = self.problem.fit_func(new_position_1)
+                new_cost_2 = self.problem.fit_func(new_position_2)
+
+                # if the new cost is smaller than the previous cost, accept a new position for pop[i]
+                if self.problem.minmax == 'min':
+                    if new_cost_1 < self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position_1
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost_1
+
+                    if new_cost_2 < self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position_2
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost_2
+
+                elif self.problem.minmax == 'max':
+                    if new_cost_1 > self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position_1
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost_1
+
+                    if new_cost_2 > self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position_2
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost_2
+
+            while True:
+                # move the droplet at the same direction with the same velocity
+                new_position = self.pop[i][self.ID_POS] + \
+                    np.random.uniform(-self.size[i],
+                                      self.size[i], self.problem.n_dims)
+                new_position = np.clip(
+                    new_position, self.problem.lb, self.problem.ub)
+                new_cost = self.problem.fit_func(new_position)
+
+                # compare new cost with current cost
+                if self.problem.minmax == 'min':
+                    if new_cost < self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost
+
+                    else:
+                        break
+                elif self.problem.minmax == 'max':
+                    if new_cost > self.pop[i][self.ID_TAR][self.ID_FIT]:
+                        self.pop[i][self.ID_POS] = new_position
+                        self.pop[i][self.ID_TAR][self.ID_FIT] = new_cost
+
+                    else:
+                        break
+
+                # reduce size of droplet depending on the soil adsorption properties
+                R = (self.soil_adsorption *
+                     self.radius[i]**self.problem.n_dims)**(1/self.problem.n_dims)
+                self.radius[i] = R
+
+                # join near droplets to each other and change the size of new droplets
+                for j in range(self.pop_size):
+                    if i == j:
+                        continue
+                    r1 = self.radius[i]
+                    r2 = self.radius[j]
+                    dist = np.linalg.norm(
+                        self.pop[i][self.ID_POS] - self.pop[j][self.ID_POS])
+
+                    if dist <= (r1 + r2):
+                        R = (r1**self.problem.n_dims + r2 **
+                             self.problem.n_dims)**(1/self.problem.n_dims)
+
+                        if self.problem.minmax == 'min':
+                            if self.pop[i][self.ID_TAR][self.ID_FIT] < self.pop[j][self.ID_TAR][self.ID_FIT]:
+                                self.pop[j][self.ID_POS] = self.pop[i][self.ID_POS]
+                                self.pop[j][self.ID_TAR][self.ID_FIT] = self.pop[i][self.ID_TAR][self.ID_FIT]
+                                self.radius[j] = R
+                                self.size[j] = self.joint_size
+
+                            else:
+                                self.pop[i][self.ID_POS] = self.pop[j][self.ID_POS]
+                                self.pop[i][self.ID_TAR][self.ID_FIT] = self.pop[j][self.ID_TAR][self.ID_FIT]
+                                self.radius[i] = R
+                                self.size[i] = self.joint_size
+
+                        if self.problem.minmax == 'max':
+                            if self.pop[i][self.ID_TAR][self.ID_FIT] > self.pop[j][self.ID_TAR][self.ID_FIT]:
+                                self.pop[j][self.ID_POS] = self.pop[i][self.ID_POS]
+                                self.pop[j][self.ID_TAR][self.ID_FIT] = self.pop[i][self.ID_TAR][self.ID_FIT]
+                                self.radius[j] = R
+                                self.size[j] = self.joint_size
+
+                            else:
+                                self.pop[i][self.ID_POS] = self.pop[j][self.ID_POS]
+                                self.pop[i][self.ID_TAR][self.ID_FIT] = self.pop[j][self.ID_TAR][self.ID_FIT]
+                                self.radius[i] = R
+                                self.size[i] = self.joint_size
+
+        # define the cost array from self.pop for use in Omit weak droplets
+        cost = []
+        self.pop = np.array(self.pop)
+        for i in range(self.pop_size):
+            cost.append(self.pop[:, self.ID_TAR][i][self.ID_FIT])
+        cost = np.array(cost)
+
+        # Omit weak droplets depending on soil adsorption
+        weak_indices = np.where((self.soil_adsorption * self.radius **
+                                self.problem.n_dims)**(1/self.problem.n_dims) < self.r_min)[0]
+        self.pop = np.delete(self.pop, weak_indices, axis=0)
+        self.radius = np.delete(self.radius, weak_indices)
+        self.size = np.delete(self.size, weak_indices)
+        self.pop_size -= len(weak_indices)
+        cost = np.delete(cost, weak_indices)
+
+        sorted_indices = np.argsort(cost)
+        self.radius = self.radius[sorted_indices]
+        self.size = self.size[sorted_indices]
+
+        # Generate new droplets depending on rain speed
+        num_new_droplets = max(1, int(self.rain_speed * self.pop_size))
+        new_droplets = self.create_population(num_new_droplets)
+        self.pop = np.vstack((self.pop, new_droplets))
+        self.radius = np.hstack(
+            (self.radius, np.full(num_new_droplets, self.init_radius)))
+        self.size = np.hstack(
+            (self.size, np.full(num_new_droplets, self.joint_size)))
+        self.pop_size += num_new_droplets
