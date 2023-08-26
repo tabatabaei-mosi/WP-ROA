fbab2fc4b288afe269a7bb3e83281fc2cbcf3c4c
Mostafa <93074310+tabatabaei-mosi@users.noreply.github.com>
2023-08-26 23:03:44 +0330

    Update utils.py
    Add NPV formula,
    Later Need to be further generalized
    
    Signed-off-by: Mostafa <93074310+tabatabaei-mosi@users.noreply.github.com>

--- src/utils.py
+++ src/utils.py
@@ -227,6 +227,25 @@ def npv_calculator(
     """
     calculate NPV by parsing RSM file (resulted from a simulation run)
 
+    The NPV formula is given by:
+    \[
+    NPV = \\sum_{t=1}^{T} \\frac{ (Q_o * r_o) - (Q_w * r_wp) + (Q_g * r_gp) - OPEX }{(1 + d)^t} - CAPEX
+    \]
+    
+    where:
+    - t: time (annular)
+    - $Q_o$: Total Oil Field Flow Rate (production)
+    - $Q_w$: Totla Water Field Flow Rate (production)
+    - $Q_g$: TOtal Gas Field Flow Rate (production)
+    
+    constants to define (dict):
+        ro: oil price ($/bbl)
+        rgp: gas price ($/bbl)
+        rwp: water production cost ($/bbl)
+        d: annual discount rate (0<d<1)
+        opex: operational expenditure ($)
+        capex: capital expenditure ($)
+
     Args:
         model_name (str): name of .DATA model
         npv_constants (dict): The constants to use in npv formula
